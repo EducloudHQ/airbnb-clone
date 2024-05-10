7f23d48de6796ac3dca1189b106d79b9508e81d6
Fonchu <pro-3ie-s@Fonchu-2.local>
2024-05-11 00:52:24 +0100

    Initial commit

---
+++ test/airbnb_clone.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AirbnbClone from '../lib/airbnb_clone-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/airbnb_clone-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AirbnbClone.AirbnbCloneStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
