7d0aa0eb5b8cdffb050e78cf10173a939ae9dd32
Fonchu <pro-3ie-s@Fonchu-2.local>
2024-05-11 04:23:34 +0100

    added building stack

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -2,6 +2,7 @@ import { AirbnbSharedStack } from "./airbnb-shared-stack";
 import { Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { UserStacks } from "./user-stack";
+import { BuildingStacks } from "./building-stack";
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props: StageProps) {
@@ -12,6 +13,11 @@ export class PipelineStage extends Stage {
      ***********************************/
     const sharedStack = new AirbnbSharedStack(this, "sharedStack");
     new UserStacks(this, "UserStacks", {
+        airbnbDatabase: sharedStack.airbnbDatabase,
+        airbnbGraphqlApi: sharedStack.airbnbGraphqlApi,
+      });
+
+      new BuildingStacks(this, "UserStacks", {
         airbnbDatabase: sharedStack.airbnbDatabase,
         airbnbGraphqlApi: sharedStack.airbnbGraphqlApi,
       });
