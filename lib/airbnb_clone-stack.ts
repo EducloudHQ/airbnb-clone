6457c990ee011f38b6ad0176a0ad6a2346683447
fonchu <fonchu.e.venyuy@gmail.com>
2024-10-31 20:40:08 +0100

    updates

--- lib/airbnb_clone-stack.ts
+++ lib/airbnb_clone-stack.ts
@@ -12,7 +12,7 @@ export class AirbnbCloneStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, "Pipeline", {
+    const pipeline = new CodePipeline(this, "PipelineTs", {
       synth: new ShellStep("synth", {
         input: CodePipelineSource.gitHub("vernyuy/airbnb-clone", "dev"),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
