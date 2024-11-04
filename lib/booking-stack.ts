705ffb0e2f57e7ec20c725c7c0f36c68962e41f0
fonchu <fonchu.e.venyuy@gmail.com>
2024-11-04 06:53:21 +0100

    updates

--- lib/booking-stack.ts
+++ lib/booking-stack.ts
@@ -1,21 +1,27 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Fn, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
 import * as appsync from "aws-cdk-lib/aws-appsync";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
-
-interface BookingStackProps extends StackProps {
-  api: appsync.GraphqlApi;
-  tableName: string;
-}
+import { Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class BookingStack extends Stack {
-  constructor(scope: Construct, id: string, props: BookingStackProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const { api, tableName } = props;
+    // const { api, tableName } = props;
+    var apiUrl = Fn.importValue("ApiUrl");
+    var tableName = Fn.importValue("DynamoDBTableName");
+    var airbnbDatabase = Table.fromTableAttributes(this, "ImportedTable",
+    {
+        tableName: tableName,
+    });
+    var api = appsync.GraphqlApi.fromGraphqlApiAttributes(this, "ImportedAPI",
+      {
+          graphqlApiId: Fn.importValue("ApiID"),
+      });
 
     const dlq = new sqs.Queue(this, "DeadLetterQueue");
     const queue = new sqs.Queue(this, "bookingQueue", {
@@ -39,7 +45,8 @@ export class BookingStack extends Stack {
         handler: "sqsConsumer.handler",
         code: lambda.Code.fromAsset(path.join(__dirname, "./lambda-fns")),
         environment: {
-          BOOKING_QUEUE_URL: queue.queueUrl
+          BOOKING_QUEUE_URL: queue.queueUrl,
+          TABLENAME: airbnbDatabase.tableName
         },
       });
     const lambdaDs = api.addLambdaDataSource("lambdaDatasource", lambdaFn);
--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -3,9 +3,9 @@ import { Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { UserStacks } from "./user-stack";
 import { BuildingStacks } from "./building-stack";
-import { ApartmentStatus } from "../src/types/appsync";
 import { ApartmentStacks } from "./apartment-stack";
 import { RatingsAndFeedbackStack } from "./ratings-feedback-stack";
+import { BookingStack } from "./booking-stack";
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props: StageProps) {
@@ -29,6 +29,9 @@ export class PipelineStage extends Stage {
         airbnbDatabase: sharedStack.airbnbDatabase,
         airbnbGraphqlApi: sharedStack.airbnbGraphqlApi,
       });
+
+
+      new BookingStack(this, "BookingStack", {});
 
       new RatingsAndFeedbackStack(this, "RatingsAndFeedbackStacks", {
         airbnbDatabase: sharedStack.airbnbDatabase,
